6c528bb8626a646e90b70a513d282780682d0196
Beny <16benyirawan@gmail.com>
2024-09-04 21:41:59 +0700

    commit

---
+++ src/main/java/com/example/regis_login/RegisLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.regis_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisLoginApplication.class, args);
+	}
+
+}
